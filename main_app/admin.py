c3b6aa790c5e8108b024e98667d37f1e0c2232ba
amwaj299 <amwaj299@hotmail.com>
2025-10-29 16:37:04 +0300

    initial commit

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Dish, Location
 
 # Register your models here.
+
+admin.site.register(Dish)
+admin.site.register(Location)
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 # Create your views here.
+from rest_framework import generics,status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Dish
+from .serializers import DishSerializer
+
+# class DisheIndex(APIView):
+#     def get(self, request):
+#         return Response({'message': 'This is the Dishes Index'})
 
 class Home(APIView):
     def get(self, request):
         return Response({'message': 'Welcome to Cultural Food API!'})
 
 class Dishes(APIView):
+    serializer_class = DishSerializer
 
     def get(self, request):
-        sample_dishes = [
-            {'name': 'Kabsa', 'origin': 'Saudi', 'description': 'Spiced rice with meat.'},
-            {'name': 'Sushi', 'origin': 'Japan', 'description': 'Fresh fish with rice.'},
-            {'name': 'Tacos', 'origin': 'Mexico', 'description': 'Tortilla with fillings.'},
-        ]
-        return Response({'dishes': sample_dishes})
+        try:
+            queryset = Dish.objects.all()
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
